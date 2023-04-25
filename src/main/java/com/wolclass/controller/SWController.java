6935cc1c9f190486a1a7e8ed25c7dddb8d589713
ho-sea <kinppq@naver.com>
2023-04-25 10:11:03 +0900

    0425_박세원_게시판글쓰기, 리스트 출력

--- src/main/java/com/wolclass/controller/SWController.java
+++ src/main/java/com/wolclass/controller/SWController.java
@@ -1,18 +1,129 @@
 package com.wolclass.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.wolclass.domain.BoardVO;
+import com.wolclass.service.SwBoardService;
 
 @Controller
 @RequestMapping("/sw/*")
 public class SWController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(SWController.class);
+
+	// 서버 시간
+	@Autowired
+	private SwBoardService service;
+
 	
-	@RequestMapping(value = "/main")
-	public void mainGET() {
-		logger.info(" mainGET() 호출 ");
+	// http://localhost:8080/sw/swWrite
+	// 글쓰기 정보 입력
+	@RequestMapping(value = "/swWrite", method = RequestMethod.GET)
+	public void registGET() throws Exception {
+		logger.info(" registGET() - 글정보 입력! ");
+		logger.info(" 연결된 view페이지 이동 ");
+		// /boards/regist.jsp
 	}
-}
+
+	// 글쓰기 정보 처리
+	@RequestMapping(value = "/swWrite", method = RequestMethod.POST)
+	public String registPOST(BoardVO vo, RedirectAttributes rttr /* Model model */) throws Exception {
+		logger.info(" registPOST()  - 글쓰기처리! ");
+		logger.info(" 글쓰기 정보: " + vo);
+		service.writeBoard(vo);
+
+		rttr.addFlashAttribute("result", "ok");
+
+		return "redirect:/sw/community";
+	}
+
+	
+	  // http://localhost:8080/sw/community 
+	  // 글 전체 조회	  
+	  @RequestMapping(value = "/community", method = RequestMethod.GET ) 
+	  public void listAllGET(@RequestParam Map<String,Object> map, Model model) throws	Exception { 
+		  logger.info(" listAllGet() 글전체 조회"); 
+	  
+		  List<BoardVO> boardList = service.selectPageNumBoTotal(map); 
+		  logger.info(" boardList {}",boardList.size()); 
+		  
+		  model.addAttribute("map", map);
+		  model.addAttribute("boardList", boardList);
+	  }
+	   
+	  
+	
+	  
+	  
+	/*
+	 * // http://localhost:8080/sw/swRead // 특정글 글정보 보기
+	 * 
+	 * @RequestMapping(value = "/swRead", method = RequestMethod.GET) public void
+	 * readGET(@RequestParam("b_no") int b_no, Model model) throws Exception {
+	 * logger.info(" readGET()  ");
+	 * 
+	 * logger.info(" �������� b_no : " + b_no);
+	 * 
+	 * service.incrementViewCnt(b_no); BoardVO resultVO = service.getBoard(b_no);
+	 * logger.info(resultVO.toString()); model.addAttribute("resultVO", resultVO); }
+	 * 
+	 * // http://localhost:8080/sw/swUpdateBo
+	 * 
+	 * @RequestMapping(value = "/swUpdateBo", method = RequestMethod.GET) public
+	 * void modifyGET(@RequestParam("b_no") int b_no, Model model) throws Exception
+	 * {
+	 * 
+	 * logger.info(" b_no : " + b_no); BoardVO vo = service.getBoard(b_no);
+	 * logger.info(vo.toString()); model.addAttribute(vo);
+	 * 
+	 * }
+	 * 
+	 * // 글 수정하기
+	 * 
+	 * @RequestMapping(value = "/##", method = RequestMethod.POST) public String
+	 * modifyPOST(BoardVO vo, RedirectAttributes rttr) throws Exception {
+	 * logger.info(vo.toString());
+	 * 
+	 * int result = service.modifyBoard(vo); if (result == 1) {
+	 * rttr.addFlashAttribute("result", "updOK"); }
+	 * 
+	 * return "redirect:/sw/community"; }
+	 * 
+	 * // 글삭제하기
+	 * 
+	 * @RequestMapping(value = "/remove", method = RequestMethod.POST) public String
+	 * removePOST(@ModelAttribute("b_no") int b_no, RedirectAttributes rttr) throws
+	 * Exception {
+	 * 
+	 * logger.info(" b_no : " + b_no);
+	 * 
+	 * int result = service.removeBoard(b_no);
+	 * 
+	 * if (result == 1) { rttr.addFlashAttribute("result", "delOK"); }
+	 * 
+	 * return "redirect:/sw/community"; }
+	 * 
+	 * // http://localhost:8080/sw/admin // 관리자페이지
+	 * 
+	 * @RequestMapping(value = "/admin") public void adminGET() {
+	 * logger.info(" admin() 호출!"); }
+	 * 
+	 * // http://localhost:8080/sw/review // 리뷰
+	 * 
+	 * @RequestMapping(value = "/review") public void reviewGET() {
+	 * logger.info(" reviewGET() 호출! "); }
+	 * 
+	 */
+	  }
---
+++ src/main/webapp/WEB-INF/views/sw/swWrite.jsp
@@ -0,0 +1,130 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style type="text/css">
+
+
+  
+ 
+.btn-backlist {
+font-weight:bold;
+font-size:15px;
+border-radius:6px; 
+ 	width: 300px; 
+ 	height: 45px;
+ background: #C9BA9B;
+}
+.btn-backlist:active {
+ background: #A58C59;
+ color: white;
+ border: 3px gray inset;
+}
+
+
+.btn-write {
+font-weight:bold;
+font-size:15px;
+border-radius:6px; 
+ 	width: 300px; 
+ 	height: 45px;
+ background: #C9BA9B;
+}
+.btn-write:active {
+ border: 3px gray inset;
+ background: #A58C59;
+ color: white;
+}
+
+
+</style>
+
+</head>
+<body>
+	<!-- header  -->
+	<%@ include file="../include/header.jsp"%>
+	<!-- header  -->
+
+	<!-- body  -->
+	<div style="margin: 45px 0;">
+	<div style="padding-left:20px; position: relative;" >
+	<hr style="width: 8%;position: absolute; ">
+		<h1 style="margin-left: 10%;line-height: inherit;">커뮤니티 - 글 작성 </h1>
+	</div>
+</div>
+	
+
+
+	<div
+		style="width: 100%; height: auto; background: white; display: inline-block; box-sizing: border-box; border: 1px">
+		<div class="col-xs-12">
+			<div class="box">
+				<div class="box-header">
+					<!-- search  -->
+					<div style="display: flex; padding-bottom: 30px; margin-left: 31%;" class="search-box">
+						<div>
+							<h3 class="box-title" style="font-size: x-large;">글 작성</h3>
+						</div>
+
+
+
+					</div>
+					<!-- content  -->
+					<form role="form" method="post">
+					<div style="display: flex;">
+						<div class="category-search" style="margin: 2px 4% 0 20%;">
+							<select style="width: 125px; height: 45px; font-size: 20px;"
+								name="b_category">
+								<option value="board">자유게시글</option>
+								<option value="recruit">모집글</option>
+							</select>
+							
+			<!--  나중에 수정해야됨   -->
+							<input type="text" name="b_writer" <%-- value="${Sessionscope.m_id}"  --%>
+							style="border: 1px solid black;">
+		<!--  나중에 수정해야됨   -->					
+						</div>
+						<div style="display: block;width: 40%;border: solid 1px gray;padding: 55px;border-radius: 5px;">
+							<div style="display: block; width: 100%;">
+								<label>글 제목</label>
+								<input style="margin-bottom: 30px;border: 1px solid gray;"
+									type="text" name="b_title" placeholder="제목을 입력해주세요.">
+							</div>
+							<div style="">
+								<label>글 내용</label>
+								<textarea rows="15" name="b_content" placeholder=" 내용을 입력해주세요." 
+								style="width:100%; overflow: initial"></textarea>
+							</div>
+							<!-- button   -->
+							<div style="display: flex; margin: 30px 0 30px; justify-content: space-between;">
+								<div>
+									<button type="submit" class="btn-write">작성하기 </button>
+								</div>
+			<!--  나중에 수정해야됨   -->
+								<div>
+									<button class="btn-backlist" onclick="location.href='./sw/community';"> 목록으로 </button>
+								</div>
+			<!--  나중에 수정해야됨   -->
+							</div>
+							<!-- button   -->
+						</div>
+						
+					</div>
+					</form>
+					</fieldset>
+					<!-- content  -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- body  -->
+
+	<!-- footer  -->
+	<%@ include file="../include/footer.jsp"%>
+	<!-- footer  -->
+
+</body>
+</html>
