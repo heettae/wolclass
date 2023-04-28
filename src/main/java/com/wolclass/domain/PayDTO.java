ce7285711be2b90b4b173b23594b95a0b62e1f7e
heettae <klaxogml@gmail.com>
2023-04-28 20:55:15 +0900

    0428 태희 - 결제하는중 2

---
+++ src/main/java/com/wolclass/domain/PayDTO.java
@@ -0,0 +1,58 @@
+//package com.wolclass.domain;
+//
+//import com.google.protobuf.Timestamp;
+//
+//import lombok.Data;
+//
+//@Data
+//public class PayDTO {
+//
+//
+//	private Timestamp selectedDate;
+//	private Integer c_no;
+//    private String t_start;
+//    private Integer pNum;
+//    private Integer point;
+//    private boolean checkedSubs;
+//    private Integer c_price;
+//    
+//    
+//  
+////    private RsrvPayVO pvo;
+////    private MemberVO mvo;
+////    private ClassVO cvo;
+////    
+//    
+//    
+//    
+////    
+////    public Integer cal() {
+//    	if(!(point>=0)) {
+//    		point = null;
+//    	}
+//    	double subs;
+//    	if(checkedSubs) {
+//    		subs=0.5;
+//    	} else {
+//    		subs=1;
+//    	}
+//    	
+////   	
+////   	int price = (int)(cvo.getC_price()*subs)+(cvo.getC_price()*pNum-1)-point;
+////
+////    		  
+////    	System.out.println(price);
+////    	return price;
+//    }
+//
+//
+//
+//
+//
+//    
+//   
+//
+//
+//    
+//
+//}
