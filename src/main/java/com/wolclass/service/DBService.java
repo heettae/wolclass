e9f7df650d9ebb37b7f12f9b104f1fa5941881a6
Limdabin <ekqls3366@naver.com>
2023-05-08 00:58:20 +0900

    0507- 다빈 네이버ing

--- src/main/java/com/wolclass/service/DBService.java
+++ src/main/java/com/wolclass/service/DBService.java
@@ -42,9 +42,6 @@ public interface DBService {
 	public void kakaoInsert(MemberVO vo) throws Exception;
 	public MemberVO kfindId(MemberVO vo) throws Exception;
 	
-	// 구글 로그인, 회원가입 - 다빈
-//	public void googleInsert(MemberVO vo) throws Exception;
-//	public MemberVO googleLogin(MemberVO vo) throws Exception;
-//	
+
 	
 }
